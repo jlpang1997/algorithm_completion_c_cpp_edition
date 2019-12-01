9a2340c7e008cdcbd0a32616815fe03cbf3c0c5d
jlpang1997 <JLPANG1997@outlook.com>
2019-12-01 08:06:59 +0800

    Project4_Linux created.

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -11,22 +11,16 @@ int main(int argc,char **argv)
 	const char *func_name[FUNC_NUM] = { "Naive","KMP","RK","BMH","finite_machine"};
 	int(*func_wrapper[FUNC_NUM])(unsigned char *,int ,unsigned char *,int) = {
 		Naive,
-		KMP,
+		KMP, 
 		RK,
-		BMH,
+		BMH,  
 		finite_machine,
 	};
 	unsigned char *input[DATA_SET][2];
 	int result[DATA_SET];
 	long time[DATA_SET];
 	int size[DATA_SET][2];
-	char filepath[100] = "D:\\algorithm\\algorithm_completion_c_cpp_edition\\input\\input_strings.txt";
-	//for (int j = 5; j < DATA_SET; j++)
-	//{
-	//	int n = 1 << (5 + 3 * j);
-	//	int m = 1 << (2 + j);
-	//	create_data(filepath, n, m);
-	//}
+	char filepath[100] = "..\\input\\input_strings.txt";
 	get_input(filepath, input);
 	char output_file[100] = "";
 	for (int i = 0; i < FUNC_NUM; i++)
@@ -35,21 +29,19 @@ int main(int argc,char **argv)
 		{
 			int n = 1 << (5 + 3 * j);
 			int m = 1 << (2 + j);
-			size[j][0] = n;
+			size[j][0] = n; 
 			size[j][1] = m;
 			LARGE_INTEGER begintime;
 			LARGE_INTEGER finishtime;
-
 			QueryPerformanceCounter(&begintime);
 			result[j] = func_wrapper[i](input[j][0], n, input[j][1], m);
 			QueryPerformanceCounter(&finishtime);
-
-			time[j] = finishtime.QuadPart - begintime.QuadPart;
+			time[j] = finishtime.QuadPart - begintime.QuadPart; 
 		}
-		strcpy(output_file, "D:\\algorithm\\algorithm_completion_c_cpp_edition\\output\\");
+		strcpy(output_file, "..\\output\\");
 		strcat(output_file, func_name[i]);
 		strcat(output_file, "\\result.txt");
 
 		get_output(output_file,result,time,size);
 	}
-}
+} 
