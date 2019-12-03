e1e26bc84b4543808a295c148b3e64cd657634d8
jinlinpang <jlpang1997@outlook.com>
2019-12-03 11:50:15 +0800

    Project4_Linux0.2

--- source/main.cpp
+++ source/main.cpp
@@ -4,23 +4,23 @@
 #include<stdio.h>
 #include<string.h>
 #include<sys/time.h>
-#define FUNC_NUM 5
+#define FUNC_NUM 4
 
 int main(int argc,char **argv)
 {	
-	const char *func_name[FUNC_NUM] = { "Naive","KMP","RK","BMH","finite_machine"};
+	const char *func_name[FUNC_NUM] = { "Naive","KMP","RK","BMH",};
 	int(*func_wrapper[FUNC_NUM])(unsigned char *,int ,unsigned char *,int) = {
 		Naive,
 		KMP, 
 		RK,
 		BMH,  
-		finite_machine,
+		// finite_machine,
 	};
 	unsigned char *input[DATA_SET][2];
 	int result[DATA_SET];
 	long time[DATA_SET];
 	int size[DATA_SET][2];
-	char filepath[100] = "..\\input\\input_strings.txt";
+	char filepath[100] = "../input/input_strings.txt";
 	get_input(filepath, input);
 	char output_file[100] = "";
 	for (int i = 0; i < FUNC_NUM; i++)
@@ -38,9 +38,9 @@ int main(int argc,char **argv)
 			gettimeofday(&end, NULL);
 			time[j] = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
 		}
-		strcpy(output_file, "..\\output\\");
+		strcpy(output_file, "../output/");
 		strcat(output_file, func_name[i]);
-		strcat(output_file, "\\result.txt");
+		strcat(output_file, "/result.txt");
 		get_output(output_file,result,time,size);
 	}
 } 
